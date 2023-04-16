1ede963e67a79a892e78494e7794454c4715ceff
hadi <mh@con>
2023-04-16 18:50:39 +0430

    swipper registered

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import { useEffect } from "react";
+import { register } from 'swiper/element/bundle';
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(()=>{
+    register()
+  },[])
   return (
     <Layout>
       <Component {...pageProps} />
