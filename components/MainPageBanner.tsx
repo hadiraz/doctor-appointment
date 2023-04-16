475806c3936ab4a0b36b1b569415ad547205d4b5
hadi <mh@con>
2023-04-16 18:19:17 +0430

    banner completed

--- components/MainPageBanner.tsx
+++ components/MainPageBanner.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import MedicalBanner from "@/public/assets/images/medical.png"
-import Image from 'next/image'
+import React from "react";
+import MedicalBanner from "@/public/assets/images/medical.png";
+import Image from "next/image";
 const MainPageBanner = () => {
   return (
-    <section className='flex w-full max-w-7xl h-screen'>
-        <div className='flex justify-center items-center'>
-            <Image src={MedicalBanner} width={70} height={70} alt='banner' />
-        </div>
+    <section className="flex flex-col items-center justify-center w-full max-w-7xl h-[88vh] md:flex-row md:h-auto">
+      <div className="flex justify-center items-center">
+        <Image src={MedicalBanner} width={400} height={400} alt="banner" />
+      </div>
+      <div className="flex flex-col mt-3 md:mt-0 justify-center">
+        <p className="text-xl font-semibold sm:text-2xl px-2">
+            {/* <span className="relative flex w-3 h-3 rounded-full bg-lime-500 mr-3">
+                <span className="absolute top-0 left-0 w-full h-full animate-ping bg-lime-500 rounded-full"></span>
+            </span> */}
+        Advanced Imaging for a <strong className="sm:text-2xl font-bold text-green-600 relative after:absolute after:h-[2px] after:w-full after:-bottom-1 after:left-0 after:bg-lightPrimary">Healthier</strong> You
+        </p>
+        <p className="text-xl font-semibold sm:text-2xl px-2 mt-5 ">
+        {/* <span className="relative flex w-3 h-3 rounded-full bg-lime-500 mr-3">
+                <span className="absolute top-0 left-0 w-full h-full animate-ping bg-lime-500 rounded-full"></span>
+            </span> */}
+        Cutting-Edge Technology for <em className=" text-sky-700 relative after:absolute after:h-[2px] after:w-full after:-bottom-1 after:left-0 after:bg-sky-200">Accurate Results</em>
+        </p>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default MainPageBanner
+export default MainPageBanner;
