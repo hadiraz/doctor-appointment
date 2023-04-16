3286b0a6ac4a265810de8dbf927bcd848e9763dc
hadi <mh@con>
2023-04-16 17:12:41 +0430

    navbar and responsive navbar compeleted

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,26 +1,74 @@
-import Image from 'next/image'
-import React, { ReactElement } from 'react'
-import logo from "@/public/assets/logo_med.png"
-const Layout = ({children} : {children : ReactElement}) => {
-  return (
-    <div className='flex w-full flex-col items-center'>
-    <header className='w-full flex justify-between items-center p-2 h-70 sticky bg-slate-500 z-10'>
-        {/* <div className='flex absolute w-full h-full blur-md top-0 left-0'></div> */}
-        <div className='flex items-center justify-center'>
-            <Image src={logo} width={50} height={50} alt="logo" />
-        </div>
-        <nav className='flex justify-between items-center'>
-            nav
-        </nav>
-        <div>
-            profile
-        </div>
-    </header>
-    <main>
-        {children}
-    </main>
-    </div>
-  )
-}
+import Image from "next/image";
+import React, { ReactElement, useState } from "react";
+import logo from "@/public/assets/logo_med.png";
+import Link from "next/link";
+import ResponsiveNav from "./ResponsiveNav";
 
-export default Layout
+const Layout = ({ children }: { children: ReactElement }) => {
+  const [navStatus, setNavStatus] = useState(false);
+  return (
+    <div className={`flex w-full flex-col items-center font-lato bg-mainBg overflow-x-hidden`}>
+      <header className="w-full flex justify-between items-center px-4 h-16 relative z-10 bg-transparent backdrop-blur-sm backdrop-filter">
+        <div className="flex items-center justify-center">
+          <Link href="/">
+            <Image
+              src={logo}
+              style={{ background: "none" }}
+              width={50}
+              height={50}
+              alt="logo"
+            />
+          </Link>
+        </div>
+        <nav className="flex justify-between items-center h-full">
+          <ul className="hidden md:flex w-full items-center h-full">
+            <li className="flex items-center justify-center transition-all duration-100 after:transition-all after:duration-300 hover:pb-1 mx-3 relative after:w-0 after:rounded-sm after:absolute after:h-[2px] after:bottom-0 hover:after:w-full after:bg-primary cursor-pointer hover:font-semibold">
+              Home
+            </li>
+            <li className="flex items-center justify-center transition-all duration-100 after:transition-all after:duration-300 hover:pb-1 mx-3 relative after:w-0 after:rounded-sm after:absolute after:h-[2px] after:bottom-0 hover:after:w-full after:bg-primary cursor-pointer hover:font-semibold">
+              About
+            </li>
+          </ul>
+        </nav>
+        <div className="hidden md:flex items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.3}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+        </div>
+        {navStatus && <div className="flex w-screen h-screen absolute top-0 left-0 backdrop-blur-sm" onClick={()=>setNavStatus(false)}></div>}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.8}
+          stroke="currentColor"
+          onClick={()=>setNavStatus(true)}
+          className="w-7 h-7 md:hidden cursor-pointer"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+          
+        </svg>
+          <ResponsiveNav className={`${!navStatus && "-right-full"}`} setMenuStatus={setNavStatus}/>  
+        
+      </header>
+      <main className="w-full h-full">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ components/ResponsiveNav.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const ResponsiveNav = ({ className , setMenuStatus }: { setMenuStatus : React.Dispatch<React.SetStateAction<boolean>> , className?: string }) => {
+  return (
+    <section
+      className={`w-[70vw] min-h-screen flex flex-col absolute top-0 right-0 bg-white rounded-tl-3xl rounded-bl-3xl transition-all duration-200 ease-in ${className}`}
+    >
+      <div className="flex w-full items-center py-4 px-3">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.8}
+          stroke="currentColor"
+          className="w-7 h-7 cursor-pointer"
+          onClick={()=>setMenuStatus(false)}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </div>
+      <div className="flex w-full items-center py-3 px-2">
+        <div className="flex w-9 h-9 rounded-full bg-white border border-gray-400 mr-2"></div>
+        <p>login please</p>
+      </div>
+      <nav className="w-full h-full flex mt-3">
+        <ul className="w-full h-full flex flex-col">
+          <li className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="17"
+              height="84"
+              fill="#8abd53"
+              className=" rotate-180 mr-3"
+            >
+              <path
+                fill=""
+                d="m5.396 28.424.303-.316A40.606 40.606 0 0 0 17 0v83.059A41.565 41.565 0 0 0 5.407 54.26c-6.933-7.215-6.938-18.614-.011-25.835Z"
+              />
+            </svg>
+            <span className="flex capitalize text-primary font-semibold">
+              home
+            </span>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export default ResponsiveNav;
