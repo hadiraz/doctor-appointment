c91b4c1405abb748abf8b1b4bc94627b416c378d
hadi <mh@con>
2023-04-18 00:26:35 +0430

    responsive styles edited for titles

--- components/MainPageBanner.tsx
+++ components/MainPageBanner.tsx
@@ -7,18 +7,18 @@ const MainPageBanner = () => {
       <div className="flex justify-center items-center">
         <Image src={MedicalBanner} width={400} height={400} alt="banner" />
       </div>
-      <div className="flex flex-col mt-3 md:mt-0 justify-center">
-        <p className="text-xl font-semibold sm:text-2xl px-2">
+      <div className="flex flex-col items-center mt-3 md:mt-0 justify-center">
+        <p className="text-xl font-extrabold md:items-start sm:text-3xl px-2 text-justify md:text-left">
             {/* <span className="relative flex w-3 h-3 rounded-full bg-lime-500 mr-3">
                 <span className="absolute top-0 left-0 w-full h-full animate-ping bg-lime-500 rounded-full"></span>
             </span> */}
-        Advanced Imaging for a <strong className="sm:text-2xl font-bold text-green-600 relative after:absolute after:h-[2px] after:w-full after:-bottom-1 after:left-0 after:bg-lightPrimary">Healthier</strong> You
+        Advanced Imaging for a <strong className="sm:text-2xl font-bold text-green-600 relative after:absolute after:h-[2px] after:w-full after:-bottom-1 after:left-0 after:bg-lightPrimary">Healthier</strong> You.
         </p>
-        <p className="text-xl font-semibold sm:text-2xl px-2 mt-5 ">
+        <p className="text-xl font-extrabold md:items-start text-justify sm:text-3xl px-2 mt-7 md:text-left">
         {/* <span className="relative flex w-3 h-3 rounded-full bg-lime-500 mr-3">
                 <span className="absolute top-0 left-0 w-full h-full animate-ping bg-lime-500 rounded-full"></span>
             </span> */}
-        Cutting-Edge Technology for <em className=" text-sky-700 relative after:absolute after:h-[2px] after:w-full after:-bottom-1 after:left-0 after:bg-sky-200">Accurate Results</em>
+        Cutting-Edge Technology for <em className=" text-sky-700 relative after:absolute after:h-[2px] after:w-full after:-bottom-1 after:left-0 after:bg-sky-200">Accurate Results</em>.
         </p>
       </div>
     </section>
