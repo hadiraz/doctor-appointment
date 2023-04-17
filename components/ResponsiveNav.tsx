c80459f465c3a751f3b535b5fd767816d3e121a6
hadi <mh@con>
2023-04-18 00:29:53 +0430

    z-index added to navbar

--- components/ResponsiveNav.tsx
+++ components/ResponsiveNav.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const ResponsiveNav = ({ className , setMenuStatus }: { setMenuStatus : React.Dispatch<React.SetStateAction<boolean>> , className?: string }) => {
   return (
     <section
-      className={`w-[70vw] min-h-screen flex flex-col absolute top-0 right-0 bg-white rounded-tl-3xl rounded-bl-3xl transition-all duration-200 ease-in ${className}`}
+      className={`w-[50vw] min-h-screen flex flex-col absolute top-0 right-0 bg-white rounded-tl-3xl rounded-bl-3xl transition-all duration-200 ease-in z-20 ${className}`}
     >
       <div className="flex w-full items-center py-4 px-3">
         <svg
@@ -32,7 +32,7 @@ const ResponsiveNav = ({ className , setMenuStatus }: { setMenuStatus : React.Di
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="17"
-              height="84"
+              height="80"
               fill="#8abd53"
               className=" rotate-180 mr-3"
             >
@@ -41,7 +41,7 @@ const ResponsiveNav = ({ className , setMenuStatus }: { setMenuStatus : React.Di
                 d="m5.396 28.424.303-.316A40.606 40.606 0 0 0 17 0v83.059A41.565 41.565 0 0 0 5.407 54.26c-6.933-7.215-6.938-18.614-.011-25.835Z"
               />
             </svg>
-            <span className="flex capitalize text-primary font-semibold">
+            <span className="flex items-center capitalize text-primary font-semibold">
               home
             </span>
           </li>
