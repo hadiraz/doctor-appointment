db16b51b7bd6f0b9bdc6300c89ac407b658756f8
hadi <mh@con>
2023-04-18 00:32:14 +0430

    modules for swiper added to the project

---
+++ scripts/build-config.ts
@@ -0,0 +1,28 @@
+module.exports = {
+    // remove modules you don't need
+    modules: [
+      'virtual',
+      'keyboard',
+      'mousewheel',
+      'navigation',
+      'pagination',
+      'scrollbar',
+      'parallax',
+      'zoom',
+      'controller',
+      'a11y',
+      'history',
+      'hash-navigation',
+      'autoplay',
+      'thumbs',
+      'free-mode',
+      'grid',
+      'manipulation',
+      'effect-fade',
+      'effect-cube',
+      'effect-flip',
+      'effect-coverflow',
+      'effect-creative',
+      'effect-cards',
+    ],
+  };
