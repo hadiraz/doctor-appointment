f1dbb5d881b2df9e5732dccfe633810725e997b7
hadi <mh@con>
2023-04-26 15:45:23 +0430

    link box border radiuse edited

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <section style={{background : ""}} className="bg-accent flex max-w-5xl z-10 relative justify-between md:justify-start rounded-lg items-center my-10 w-full py-2 px-3">
         <span className="blur-lg w-full h-full absolute top-1 left-1 flex rounded-lg -z-10 bg-accent"></span>
         <span className="w-full h-full absolute top-0 left-0 flex rounded-lg z-[-1] bg-accent opacity-40"></span>
-          <Image style={{objectFit:"cover" , objectPosition:"center"}} className="absolute rounded-xl -z-10 bg-slate-400" src={Nuclear} fill={true} alt="nuclear"/>
+          <Image style={{objectFit:"cover" , objectPosition:"center"}} className="absolute rounded-lg -z-10 bg-slate-400" src={Nuclear} fill={true} alt="nuclear"/>
         <p className="text-lg md:text-2xl font-bold text-text">Lets start here</p>
         <button className=" bg-lime-500 font-bold rounded-xl flex justify-center items-center p-2 text-white shadow-xl mx-2 ml-5">Book appointment</button>
       </section>
