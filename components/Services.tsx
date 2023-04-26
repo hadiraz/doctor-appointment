8eaf7f83d8b23b2cfdfafea56da671ffa73db5f8
hadi <mh@con>
2023-04-26 20:37:54 +0430

    services component created

---
+++ components/Services.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section className='grid w-full grid-cols-1'>
+        <div className='flex rounded-xl bg-zinc-700 w-52 h-40'>
+
+        </div>
+    </section>
+  )
+}
+
+export default Services
