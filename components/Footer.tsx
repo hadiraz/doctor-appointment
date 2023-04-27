ad240ff24c60443ee80a8eeaa55e026bbad4085b
hadi <hadiraziei1@gmail.com>
2023-04-27 19:26:23 +0430

    footer completed icons must be added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div></div>
+    <footer className='flex items-center justify-center w-full bg-[#d6d6d6] '>
+      <div className='flex flex-col md:flex-row justify-between items-center w-full max-w-7xl p-2'>
+      <section className='flex justify-center items-center flex-col w-full md:w-1/2'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, consequatur. Ea amet a consequatur soluta reiciendis voluptates, assumenda iste sed hic voluptatem explicabo repellendus, dicta quaerat. Inventore quibusdam repellendus dolore!.</p>
+        <div className='flex mt-10'>
+          <span className='flex items-center justify-center'>icon</span>
+        </div>
+      </section>
+      <section className='flex justify-center items-center w-full md:w-1/2'>
+        <ul className='flex'>
+          <li>2</li>
+          <li>3</li>
+          <li>5</li>
+        </ul>
+      </section>
+      </div>
+    </footer>
   )
 }
 
