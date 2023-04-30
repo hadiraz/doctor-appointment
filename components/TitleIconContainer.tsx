ec4c466c45a963798edd40ce966340ffd1c68675
hadi <hadiraziei1@gmail.com>
2023-04-30 14:00:23 +0430

    section's bg component created

---
+++ components/TitleIconContainer.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from "react";
+
+const TitleIconContainer = ({children , classes} : {children : ReactElement , classes ?: string}) => {
+  return (
+    <span className={`${classes} flex justify-center transition-all duration-200 items-center rounded-5xl bg-buttons p-4 rounded-[18px] shadow-xl shadow-buttonShadows`}>
+      {children}
+    </span>
+  );
+};
+
+export default TitleIconContainer;
