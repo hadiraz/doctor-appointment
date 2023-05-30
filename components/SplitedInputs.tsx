37f32065e7c94fdce16fdb08ff58d928b3bd4aeb
hadi <hadiraziei1@gmail.com>
2023-05-30 22:51:43 +0330

    paste function completed and digits set method changed

--- components/SplitedInputs.tsx
+++ components/SplitedInputs.tsx
@@ -1,5 +1,6 @@
 import React, {
   ChangeEvent,
+  ClipboardEvent,
   KeyboardEvent,
   useEffect,
   useRef,
@@ -15,42 +16,53 @@ const SplitedInputs = ({
   stringCode,
   setStringCode,
 }: InputsConfig) => {
-  const [items, setItems] = useState<number[]>([]);
+  const [items, setItems] = useState<{id : number , value :string}[]>([]);
+  const [pasteStatus, setPasteStatus] = useState<boolean>(false);
   const FirstInputRef = useRef<HTMLInputElement>(null);
+  const InputContainersRef = useRef<HTMLSpanElement>(null);
   useEffect(() => {
-    let newArr: number[] = [];
+    let newArr: {id : number , value :string}[] = [];
     for (let i = 0; i < inputsNumber; i++) {
-      newArr[i] = i;
+      newArr[i] = {id : i , value : ""};
     }
     setItems(newArr);
-}, [inputsNumber]);
-useEffect(() => {
-    FirstInputRef.current?.focus();
+  }, [inputsNumber]);
+  useEffect(() => {
+    setStringCode(items.join(""))
   }, [items]);
 
-  const setCodeDigit = (inputValue: string, id: number) => {
-    setStringCode((prev) => {
-      const newArrayCode = prev.split("").map((value, key) => {
-        if (key === id) {
-        } else return value;
-        return inputValue;
+  useEffect(() => {
+    items.length > 0 && FirstInputRef.current?.focus();
+  }, [items.length]);
+
+  const setCodeDigit = (inputValue: string, inputId: number) => {
+    setItems(prev => {
+      const newItems = prev.map(({value , id}, key) => {
+        if (id === inputId) {
+          return {id , value : inputValue}
+        }else return {id , value} 
       });
-      return newArrayCode.join("");
-    });
+      return newItems
+    })
   };
 
   const inputChangeHandler = (e: ChangeEvent<HTMLInputElement>, id: number) => {
-    const enteredCharacter: string = e.target.value.split("")[0];
-    const nextSibling = e.target.nextElementSibling as HTMLInputElement | null;
-    const checkDigit = /\d/.test(enteredCharacter);
-    console.log(enteredCharacter)
-    if (checkDigit) {
-      stringCode.length === inputsNumber
-        ? setCodeDigit(enteredCharacter, id)
-        : setStringCode((prev) => prev + enteredCharacter);
-      nextSibling?.focus();
-    }
+    if(items.length <= inputsNumber && !pasteStatus && e.target.value.length < 2) {checkDigits(e.target.value , id , e)}
   };
+  const checkDigits = ( digit: string , id: number, e?: ChangeEvent<HTMLInputElement> ) => {
+    let enteredCharacter: string = digit;
+      const checkDigit = /\d/.test(enteredCharacter);
+      let nextSiblingElement : HTMLInputElement | null = null ;
+      if (e) {
+        nextSiblingElement = e.target.nextElementSibling as HTMLInputElement | null ;
+      }
+      if (!checkDigit) {
+        enteredCharacter = ""
+        console.log(digit , 55)
+      }
+      digit && setCodeDigit(enteredCharacter, id);
+      if(checkDigit) nextSiblingElement?.focus();
+    };
 
   const inputKeyDownHandler = (
     e: KeyboardEvent<HTMLInputElement>,
@@ -58,55 +70,65 @@ useEffect(() => {
   ) => {
     const target = e.target as HTMLInputElement | null;
     const nextElement = target?.nextElementSibling as HTMLInputElement | null;
-    if (e.key !== "Backspace" && String(target?.value) && String(!nextElement?.value)) {
+    if (e.key !== "Backspace" && String(target?.value) && String(!nextElement?.value) ) {
       nextElement?.focus();
     }
     if (target && e.key === "Backspace") {
-      console.log(stringCode)
-      const prevSibling =
-        target.previousElementSibling as HTMLInputElement | null;
+      console.log("hiii")
+      const prevSibling = target.previousElementSibling as HTMLInputElement | null;
       setCodeDigit("", id);
+      if(!target.value && id > 0) setCodeDigit("" , id-1)
+      if(!target.value && id === 0) setCodeDigit("" , id)
       prevSibling && prevSibling.focus();
-      return;
+    }
+  };
+
+  const inputPasteHandler = (e: ClipboardEvent<HTMLInputElement>) => {
+    if (e.clipboardData && e.clipboardData.getData("text").length === items.length ) {
+      setPasteStatus(true)
+      const enteredData = e.clipboardData.getData("text");
+      enteredData.split("").forEach((value,index)=>{
+        checkDigits(value , index)
+        index === items.length - 1 && setPasteStatus(false)
+      })
+
     }
   };
   return (
     <>
-      {items.map((value, key) => {
-        const splitedStringCode = stringCode.split("")[key];
+    {/* // <span ref={InputContainersRef} className="flex items-center"> */}
+      {items.map(({value , id}, key) => {
         if (key === 0) {
           return (
             <input
               ref={FirstInputRef}
-              onChange={(e) => inputChangeHandler(e, key)}
-              onKeyDown={(e) => inputKeyDownHandler(e, key)}
-              value={Number(splitedStringCode) || Number(splitedStringCode) === 0 ? splitedStringCode : ""}
-              key={key}
+              onChange={(e) => inputChangeHandler(e, id)}
+              onKeyDown={(e) => inputKeyDownHandler(e, id)}
+              onPaste={(e) => inputPasteHandler(e)}
+              value={value}
+              key={id}
               inputMode="numeric"
               type="text"
               autoComplete="one-time-code"
-              pattern="\d{1}"
               className="border-b-text text-lg bg-transparent w-6 text-center appearance-none mx-1 flex items-center justify-center border-b-[1px] outline-none border-gray-400 focus:border-b-emerald-300 focus:border-b-[2px]"
             />
           );
-        
         } else {
           return (
             <input
-              onChange={(e) => inputChangeHandler(e, key)}
-              onKeyDown={(e) => inputKeyDownHandler(e, key)}
-              value={Number(splitedStringCode) || Number(splitedStringCode) === 0  ? splitedStringCode : ""}
-              key={key}
+              onChange={(e) => inputChangeHandler(e, id)}
+              onKeyDown={(e) => inputKeyDownHandler(e, id)}
+              value={value}
+              key={id}
               inputMode="numeric"
               type="text"
-              maxLength={1}
               autoComplete="one-time-code"
-              pattern="\d{1}"
               className="border-b-text text-lg bg-transparent w-6 text-center appearance-none mx-1 flex items-center justify-center border-b-[1px] outline-none border-gray-400 focus:border-b-emerald-300 focus:border-b-[2px]"
             />
           );
         }
       })}
+    {/* // </span> */}
     </>
   );
 };
