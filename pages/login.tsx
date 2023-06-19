64478787d7a93c51a0cb1208854d7d9c7ee3958b
Hadi Raziei <hadiraziei1@gmail.com>
2023-06-20 00:15:51 +0330

    login responsive completed

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,15 +1,22 @@
 import { glassStyle } from "@/public/styles/style";
+import Image from "next/image";
 import React from "react";
 import styled from "styled-components";
-
-const Input = styled.input`
-  
-`;
+import Doctor from "@/public/assets/images/doctor-login-min.png";
+const Input = styled.input``;
 const Login = () => {
   return (
-    <section className="flex w-full h-screen relative items-center justify-between bg-gray-100">
-      <div className="flex w-2/3 justify-end items-center relative h-full bg-primary rounded-tr-[30px] rounded-br-[30px]">
-        <form className="flex min-w-[250px] w-2/5 flex-col absolute translate-x-1/2 rounded-xl p-8 bg-white">
+    <section className="flex w-full h-screen relative items-center justify-center md:justify-between bg-gray-100">
+      <div className="hidden md:flex w-2/3 items-center relative h-full bg-primary rounded-tr-[30px] rounded-br-[30px]">
+        <div className="flex flex-col w-full items-center ml-4 mr-20">
+          <div className="flex flex-col w-full">
+            <h3 className="text-center font-extrabold text-3xl text-white mb-5">Welcome to Login page</h3>
+          </div>
+          <Image src={Doctor} alt="Image" width={200} height={200} />
+        </div>
+
+      </div>
+        <form className="flex min-w-[250px] max-w-[400px] w-full mx-3 md:w-1/3 flex-col md:-translate-x-1/2 rounded-xl p-8 bg-white">
           <h4 className="font-extrabold text-lg w-full text-center mb-5">
             Nikan Nuclear
           </h4>
@@ -32,11 +39,13 @@ const Login = () => {
               Phone number
             </label>
           </div>
-          <button type="submit" className="w-full rounded-xl bg-primary text-white py-2 px-3 mt-5 shadow-lg shadow-[#]">
+          <button
+            type="submit"
+            className="w-full rounded-xl bg-primary text-white py-2 px-3 mt-5 shadow-lg shadow-[#]"
+          >
             Login | Register
           </button>
         </form>
-      </div>
     </section>
   );
 };
