155b7e580b59c48b66740d404ee3005a537faee6
Hadi Raziei <hadiraziei1@gmail.com>
2023-08-26 18:56:48 +0330

    unnecessary codes removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import Layout from "@/components/layout/Layout";
+import AppointmentContext, {
+  AppointmentCreateContext, setPhoneNumberContext,
+} from "@/context/appointment/LoginContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
-import React from "react";;
+import { useContext, useEffect } from "react";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
+    <AppointmentContext>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </AppointmentContext>
   );
 }
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import TeamSwiperContainer from "@/components/swiper/TeamSwiperContainer";
 import Image from "next/image";
 import Nuclear from "@/public/assets/images/nuclear-image.jpg";
 import Services from "@/components/mainPage/Services";
+import { useContext, useEffect } from "react";
+import { AppointmentCreateContext } from "@/context/appointment/LoginContext";
+import Link from "next/link";
 export default function Home() {
   return (
     <section className="flex w-full flex-col items-center min-h-screen max-w-7xl px-3">
@@ -12,7 +15,6 @@ export default function Home() {
         style={{ background: "" }}
         className="bg-accent flex max-w-5xl z-[5] relative justify-between md:justify-start rounded-lg items-center my-10 w-full py-2 px-3"
       >
-        {/* <span className="blur-lg w-full h-full absolute top-1 left-1 flex rounded-lg -z-10 bg-accent"></span> */}
         <span className="w-full h-full absolute top-0 left-0 flex rounded-lg z-[-1] bg-accent opacity-40"></span>
         <Image
           style={{ objectFit: "cover", objectPosition: "center" }}
@@ -25,9 +27,9 @@ export default function Home() {
           <p className="text-2xl sm:text-3xl font-bold text-white mb-5 sm:mb-0">
             Lets start here
           </p>
-          <button className=" bg-lime-500 font-bold rounded-xl flex justify-center items-center p-2 text-white shadow-xl mx-2 ml-5">
+          <Link href="/appointment" className=" bg-lime-500 font-bold rounded-xl flex justify-center items-center p-2 text-white shadow-xl mx-2 ml-5">
             Book appointment
-          </button>
+          </Link>
         </div>
       </section>
       <Services/>
